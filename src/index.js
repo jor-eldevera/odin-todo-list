5e397f3ba02f6ab6c64348ad3e6a800b030998bd
jor-eldevera <jorell.devera@gmail.com>
2023-02-09 17:11:20 -0800

    Create Project and TodoItem skeletons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import "./style.css"
+
+class Project {
+    todoItems = [];
+
+    constructor(title) {
+        this.title = title;
+    }
+}
+
+class TodoItem {
+    constructor(title, description, dueDate, priority) {
+
+    }
+}
