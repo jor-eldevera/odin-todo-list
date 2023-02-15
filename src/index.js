154b0bf44b609b2722cf840661747b63fa515e84
jor-eldevera <jorell.devera@gmail.com>
2023-02-14 21:36:51 -0800

    Add function addTodoItem to Project Add function searchItems to Project Add fields to TodoItem's constructor

--- src/index.js
+++ src/index.js
@@ -6,10 +6,62 @@ class Project {
     constructor(title) {
         this.title = title;
     }
+
+    get todoItems() {
+        return this.todoItems;
+    }
+
+    /**
+     * Adds a TodoItem to this project's todoItems array
+     * @param {*} title - The title of the TodoItem
+     * @param {*} description - The description of the TodoItem
+     * @param {*} dueDate - The due date of the TodoItem
+     * @param {*} priority - The priority level of the TodoItem
+     * @returns null if there's error, undefined if successful
+     */
+    addTodoItem(title, description, dueDate, priority) {
+        // Check if a todo item with the same title is already in this list
+        if (this.searchItems(title)) {
+            console.log("There is already an item in this list with the title: " + title);
+            return;
+        }
+
+        // Check if todo item has a title
+        if (!title || title === "") {
+            console.log("Todo item must have a title");
+            return;
+        }
+
+        this.todoItems.push(new TodoItem(title, description, dueDate, priority));
+    }
+
+    /**
+     * Searches for a TodoItem in this project's todoItems array
+     * @param {*} title - The title of the TodoItem
+     * @returns The found TodoItem or null if the item isn't found
+     */
+    searchItems(title) {
+        for (let item of this.todoItems) {
+            if (item.title === title) {
+                return item;
+            }
+        }
+
+        return null;
+    }
 }
 
 class TodoItem {
     constructor(title, description, dueDate, priority) {
-
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
     }
 }
+
+let newProject = new Project();
+newProject.addTodoItem("testTitle", "testDesc", "January 14th, 2023", 2);
+newProject.addTodoItem("TestTitle", "TestDesc", "January 14th, 2023", 3);
+console.log(newProject.addTodoItem("TESTTitle", "TestDesc", "January 14th, 2023", 2));
+console.log(newProject.todoItems);
